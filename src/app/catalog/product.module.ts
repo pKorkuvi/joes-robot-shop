7cc76d54ecbedc328cd114b50223244b50e512bc
pKorkuvi <pkorkuvi10@gmail.com>
2025-01-30 15:05:43 -0800

    Implement binding on catalog page

---
+++ src/app/catalog/product.module.ts
@@ -0,0 +1,9 @@
+export interface IProduct       {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+    imageName: string;
+    category: string;
+    discount:   Number
+}
